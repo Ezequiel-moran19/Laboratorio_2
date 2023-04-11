3725899a0c46666cf8157288a9c66202b2abfad7
Ezequiel Moran <moran.margot@hotmail.com>
2023-04-10 21:19:32 -0300

    Ejercicio Sumador

---
+++ Ejercicios_4/Sumador/Sumador/Sumador.cs
@@ -0,0 +1,48 @@
+﻿namespace Sumador_biblio
+{
+    public class Sumador
+    {
+        private int cantiddadSumas;
+
+        public Sumador(int cantiddadSumas)
+        {
+            this.cantiddadSumas = cantiddadSumas;
+        }
+        public Sumador() : this(0)
+        {
+
+        }
+        public long Sumar(long a, long b)
+        {
+            this.cantiddadSumas++;
+            return a + b;
+        }
+        public string Sumar(string a, string b)
+        {
+            this.cantiddadSumas++;
+            return a + b;
+        }
+
+        public static explicit operator int(Sumador s1)
+        {
+            return s1.cantiddadSumas;
+        }
+        
+        public static long operator +(Sumador s1, Sumador s2)
+        {
+            return s1.cantiddadSumas + s2.cantiddadSumas;
+        }
+
+        public static bool operator |(Sumador s1, Sumador s2)
+        {
+            if (s1.cantiddadSumas == s2.cantiddadSumas)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Ejercicios_4/Sumador/Vista/Program.cs
@@ -0,0 +1,25 @@
+﻿using Sumador_biblio;
+
+namespace Vista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador sumador = new Sumador(2);    
+            Sumador s1 = new Sumador(5);
+            Sumador s2 = new Sumador(5);
+
+            int entero = (int)sumador;
+            Console.WriteLine(s1.Sumar(5, 5));
+
+            Console.WriteLine(s2.Sumar("Hola", "Mundo"));
+
+            Console.WriteLine(s1 + s2);
+
+            Console.WriteLine(s1 | s2);
+
+            Console.WriteLine($"Cantidad de sumas: {entero}");
+        }
+    }
+}
