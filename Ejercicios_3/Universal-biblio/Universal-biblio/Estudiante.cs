3b8c8e1c0f9c6d3e246c22ac01a272bbdc477db2
Ezequiel Moran <moran.margot@hotmail.com>
2023-04-15 15:38:28 -0300

    Tarea Universal

---
+++ Ejercicios_3/Universal-biblio/Universal-biblio/Estudiante.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+
+namespace Universal_biblio
+{
+    public class Estudiante
+    {
+        private string nombre;
+        private string apellido;
+        private string legajo;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+        private static Random random;
+
+        static Estudiante()
+        {
+            random = new Random();
+        }
+        public Estudiante(string nombre, string apellido, string legajo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.legajo = legajo;
+        }
+        public void SetNotaPrimerParcial(int nota)
+        {
+            this.notaPrimerParcial = nota;
+        }
+        public void SetNotaSegundoParcial(int nota)
+        {
+            this.notaSegundoParcial = nota;
+        }
+        public double CalularNotaFinal()
+        {
+            if (this.notaPrimerParcial >= 4 && this.notaSegundoParcial >= 4)
+            {
+                return random.Next(6, 11);
+            }
+            return -1;
+        }
+        private float CalularPromedio()
+        {
+            return (this.notaPrimerParcial + (float)this.notaSegundoParcial) / 2;
+        }
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            double notaFinal = CalularNotaFinal();
+            sb.AppendLine($"Nombre: {this.nombre}, Apellido: {this.apellido}, Legajo: {this.legajo}");
+            sb.AppendLine($"Primer Parcial: {this.notaPrimerParcial}, Segundo Parcial: {this.notaSegundoParcial}");
+            sb.AppendLine($"Promedio: {CalularPromedio()}");
+
+            if (notaFinal != -1)
+            {
+                sb.AppendLine($"Nota Final: {notaFinal}");
+            }
+            else
+            {
+                sb.AppendLine("Alumno desaprobado");
+            }
+            return sb.ToString();
+        }
+
+
+    }
+}
---
+++ Ejercicios_3/Universal-biblio/Universal-consola/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Universal_biblio;
+
+namespace Universal_consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "El ejemplo universal";
+
+            Estudiante A1 = new Estudiante("Ezequiel", "Moran", "1234");
+            Estudiante A2 = new Estudiante("Lorenzo", "Lama", "1000");
+            Estudiante A3 = new Estudiante("Nicolas", "Tagliafico", "1111");
+
+            A1.SetNotaPrimerParcial(6);
+            A1.SetNotaSegundoParcial(5);
+
+            A2.SetNotaPrimerParcial(2);
+            A2.SetNotaSegundoParcial(3);
+
+            A3.SetNotaPrimerParcial(7);
+            A3.SetNotaSegundoParcial(9);
+
+            Console.WriteLine(A1.Mostrar());
+            Console.WriteLine(A2.Mostrar());
+            Console.WriteLine(A3.Mostrar());
+        }
+    }
+}
