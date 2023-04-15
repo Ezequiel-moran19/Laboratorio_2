cec3b756890890fb3d1fd2ebd5e32c21356a697a
Ezequiel Moran <moran.margot@hotmail.com>
2023-04-15 15:25:00 -0300

    Tarea Primavera

---
+++ Ejercicios_3/Primavera-biblio/Primavera-biblio/Persona.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+
+namespace Primavera_biblio
+{
+    public class Persona
+    {
+        private string nombre;
+        private DateTime fechaDeNacimiento;
+        private string dni;
+
+        public Persona(string nombre, DateTime fechaDeNacimiento, string dni)
+        {
+            this.nombre = nombre;
+            this.fechaDeNacimiento = fechaDeNacimiento;
+            this.dni = dni;
+        }
+        public DateTime SetFechaNacimiento()  
+        {
+            return this.fechaDeNacimiento;
+        } 
+
+        public string GetNombre() 
+        {
+            return nombre;
+        }
+        public string GetFechaDeNacimiento()
+        {
+            return fechaDeNacimiento.ToShortDateString();
+        }
+        public string GetDni() 
+        {
+            return dni;
+        }
+       
+        private int CalcularEdad() 
+        {
+            DateTime hoy = DateTime.Today;
+            int edad = hoy.Year - fechaDeNacimiento.Year;
+            if (fechaDeNacimiento > hoy.AddYears(-edad))  
+            {
+                edad--;
+            }
+            return edad;
+        }
+        public string EsMayorDeEdad()
+        {
+       
+            if (CalcularEdad() >= 18)
+            {
+                return "Es mayor de edad";
+            }
+            else
+            {
+                return "Es menor";
+            }
+        }
+        public string Mostrar() 
+        {
+            StringBuilder sp = new StringBuilder();
+            sp.AppendLine($"Nombre: {this.nombre}");
+            sp.AppendLine($"Edad Actual: {CalcularEdad()}");
+            sp.AppendLine($"DNI : {this.dni}");
+            return sp.ToString();
+        }
+    }
+}
---
+++ Ejercicios_3/Primavera-biblio/Primavera-consola/Program.cs
@@ -0,0 +1,47 @@
+﻿using Primavera_biblio;
+
+namespace Primavera_consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Persona persona1 = new Persona("Juan Perez", new DateTime(1990, 5, 15), "12345678");
+            Persona persona2 = new Persona("Maria Gomez", new DateTime(2005, 10, 30), "23456789");
+            Persona persona3 = new Persona("Pedro Rodriguez", new DateTime(1985, 3, 22), "34567890");
+
+            Console.WriteLine(persona1.Mostrar());
+            Console.WriteLine(persona2.Mostrar());
+            Console.WriteLine(persona3.Mostrar());
+            
+            Console.WriteLine("\nMayores de edad:");
+            if (persona1.EsMayorDeEdad() == "Es mayor de edad")
+            {
+                Console.WriteLine(persona1.GetNombre());
+            }
+            if (persona2.EsMayorDeEdad() == "Es mayor de edad")
+            {
+                Console.WriteLine(persona2.GetNombre());
+            }
+            if (persona3.EsMayorDeEdad() == "Es mayor de edad")
+            {
+                Console.WriteLine(persona3.GetNombre());
+            }
+                    
+            Console.WriteLine("\nMenores de edad:");
+            if (persona1.EsMayorDeEdad() == "Es menor")
+            {
+                Console.WriteLine(persona1.GetNombre());
+            }
+            if (persona2.EsMayorDeEdad() == "Es menor")
+            {
+                Console.WriteLine(persona2.GetNombre());
+            }
+            if (persona3.EsMayorDeEdad() == "Es menor")
+            {
+                Console.WriteLine(persona3.GetNombre());
+            } 
+        }
+    }
+}
