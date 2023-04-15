6396b9117493775ecd225f167fef89ea54996ea0
Ezequiel Moran <moran.margot@hotmail.com>
2023-04-15 15:40:33 -0300

    Tarea Invento Argentina

---
+++ Ejercicios_3/Invento argentino/Invento argentino/Boligrafo.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Invento_argentino
+{
+    public class Boligrafo
+    {
+        public const short cantidadTintaMaxima = 100;
+        private short tinta;
+        private ConsoleColor color;
+
+        public Boligrafo(ConsoleColor color, short tinta)
+        {
+            this.tinta = tinta;
+            this.color = color;
+        }
+
+        public short GetTinta() 
+        {
+            return this.tinta;
+        } 
+        public ConsoleColor GetColor() 
+        {
+            return this.color; 
+        }
+        private void SetTinta(short tinta) 
+        {
+            int cantidadTinta = this.tinta + tinta;
+            if (cantidadTinta >= 0 && cantidadTinta <= cantidadTintaMaxima) 
+            {
+                this.tinta = tinta;
+            }
+        }
+        public string Pintar(short gasto,string dibujo) 
+        {
+            int tintaUsada = Math.Min(gasto, tinta);
+            SetTinta((short)-tintaUsada);
+            Console.ForegroundColor = color;
+            return new string('*', tintaUsada);
+        }
+
+        public void Recargar() 
+        {
+            SetTinta((short)(cantidadTintaMaxima - this.tinta));
+        }
+    }
+}
---
+++ Ejercicios_3/Invento argentino/Vista/Program.cs
@@ -0,0 +1,34 @@
+﻿using Invento_argentino;
+
+namespace Vista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Boligrafo boliAzul = new Boligrafo(ConsoleColor.Blue, 100);
+            Boligrafo boliRed = new Boligrafo(ConsoleColor.Red, 50);
+
+            Console.WriteLine($"Bolígrafo azul - Tinta: {boliAzul.GetTinta()}, Color: {boliAzul.GetColor()}");
+            Console.WriteLine($"Bolígrafo rojo - Tinta: {boliRed.GetTinta()}, Color: {boliRed.GetColor()}");
+
+            Console.WriteLine("-----------------------------------------------------------------");
+            boliAzul.Pintar(10, "*");
+            Console.WriteLine($"Bolígrafo azul - Tinta: {boliAzul.GetTinta()}, Color: {boliAzul.GetColor()}");
+            Console.WriteLine();
+
+            Console.WriteLine("-----------------------------------------------------------------");
+            boliRed.Pintar(30, "*");
+            Console.WriteLine($"Bolígrafo rojo - Tinta: {boliRed.GetTinta()}, Color: {boliRed.GetColor()}");
+            Console.WriteLine();
+
+            Console.WriteLine("-----------------------------------------------------------------");
+            boliAzul.Recargar();
+            Console.WriteLine($"Bolígrafo azul - Tinta: {boliAzul.GetTinta()}, Color: {boliAzul.GetColor()}");
+
+            Console.WriteLine("-----------------------------------------------------------------");
+            boliAzul.Recargar();
+            Console.WriteLine($"Bolígrafo Roja - Tinta: {boliRed.GetTinta()}, Color: {boliRed.GetColor()}");
+        }
+    }
+}
