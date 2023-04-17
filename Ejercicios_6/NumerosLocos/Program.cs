1bc7fa87a80b0dfd33541ac63e8c8765bd52c687
Ezequiel Moran <moran.margot@hotmail.com>
2023-04-17 11:02:45 -0300

    Tarea Numeros locos

---
+++ Ejercicios_6/NumerosLocos/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace NumerosLocos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int>();
+            Random random = new Random();
+
+            while (numeros.Count < 20)
+            {
+                int num = 0;
+                do
+                {
+                    num = random.Next(-100, 100);
+                } while (num == 0 || numeros.Contains(num));
+                numeros.Add(num);
+            }
+
+            Console.WriteLine("Numero tal como fueron ingresado: ");
+            foreach (int num in numeros)
+            {
+                Console.WriteLine($"El numero es: {num}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("=========================================");
+            Console.WriteLine("Positivos ordenados en forma decreciente:");
+            numeros.Sort((num, b) => b.CompareTo(num));
+            foreach (int num in numeros)
+            {
+                if (num > 0)
+                {
+                    Console.Write($"\nDecreciente {num}");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("=========================================");
+            Console.WriteLine("Negativos ordenados en forma creciente:");
+            numeros.Sort((num, b) => b.CompareTo(num));
+            foreach (int num in numeros)
+            {
+                if (num < 0)
+                {
+                    Console.Write($"\nCrecientes {num}");
+                }
+            }
+
+        }
+    }
+}
